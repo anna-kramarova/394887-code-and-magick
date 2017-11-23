61fdd5b240eed0fcd9207f4eb07c903bc4bf8ebb
Anna Kramarova <kramarovaaa@gmail.com>
2017-11-23 22:27:52 +0200

    Added functions "drawBar" and "getRandomColor" to bar chart

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,31 @@
 window.renderStatistics = function (ctx, names, times) {
 
+// В этой функции тень и текст привязаны к размерам и расположению блока, мы ее не оговаривали, но я решила попрактиковаться
+/*
+  var blockX = 110;
+  var blockY = 10;
+  var blockWidth = 420;
+  var blockHeight = 270;
+  var shadowShift = 10;
+
+  var drawBlock = function() {
+
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.fillRect(blockX + shadowShift, blockY + shadowShift, blockWidth, blockHeight);
+
+    ctx.fillStyle = 'white';
+    ctx.strokeRect(blockX, blockY, blockWidth, blockHeight);
+    ctx.fillRect(blockX, blockY, blockWidth, blockHeight);
+
+    ctx.fillStyle = '#000';
+    ctx.font = '14px PT Mono';
+    ctx.fillText('Ура, вы победили!', blockX + 10, blockY + 20);
+    ctx.fillText('Список результатов:', blockX + 10, blockY + 40);
+  }
+
+  drawBlock();
+*/
+
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
 
@@ -7,19 +33,32 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.strokeRect(100, 10, 420, 270);
   ctx.fillRect(100, 10, 420, 270);
 
-
   ctx.fillStyle = '#000';
   ctx.font = '14px PT Mono';
   ctx.fillText('Ура, вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 
 
-  var max = -1;
-  for(var i = 0 ; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
+  var getMaxNumber = function(array) {
+    var max = -1;
+    for(var i = 0 ; i < array.length; i++) {
+      var time = array[i];
+      if (time > max) {
       max = time;
+      }
     }
+    return max;
+  }
+  var max = getMaxNumber(times);
+
+
+//я пыталась, но не придумала ничего лучше :(
+  var getRandomColor = function() {
+    var opacity = Math.random();
+    var getRandomInt = function() {
+    return Math.floor(Math.random() * (256 - 0)) + 0;
+    }
+    ctx.fillStyle = 'rgba(' + getRandomInt() + ',' + getRandomInt() + ',' + getRandomInt() + ',' + opacity + ')';
   }
 
 
@@ -28,21 +67,28 @@ window.renderStatistics = function (ctx, names, times) {
   var barWidth = 40;
   var indent = 50 + barWidth;
   var initialX = 120;
-  var initialY = 80;
+  var initialY = 250 ;
   var lineHeight = 20;
 
 
-  for(var i = 0; i < times.length; i++) {
+  var drawBar = function() {
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(Math.round(times[i]), initialX + indent * i, initialY - 5);
-    ctx.fillText(names[i], initialX + indent * i, initialY + histogramHeight + lineHeight);
+
+    ctx.fillText(Math.round(times[i]), initialX + indent * i, initialY - times[i] * step - 10);
+
+    ctx.fillText(names[i], initialX + indent * i, initialY + lineHeight);
 
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, 1)';
+      getRandomColor();
     }
-
-    ctx.fillRect(initialX + indent * i, initialY, barWidth, times[i] * step);
+    ctx.fillRect(initialX + indent * i, initialY, barWidth, times[i] * step * (-1));
   }
+
+
+  for(var i = 0; i < times.length; i++) {
+    drawBar(i);
+  }
+
 }
